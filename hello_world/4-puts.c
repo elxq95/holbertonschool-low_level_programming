b5682f1eb83f34fa41b496eeea205ac03a23aaae
elxq95 <leongxiqern12@gmail.com>
2024-06-17 12:00:24 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main- Entry point
+ * Description: A C program that prints exactly
+ * "\"Programming is like building a multilingual puzzle"
+ * using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
