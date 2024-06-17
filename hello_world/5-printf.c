0b01979e9ce8c2a4ba451466a96ee195a19eae3a
elxq95 <leongxiqern12@gmail.com>
2024-06-17 12:06:34 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** main is the  Entry point
+ * This is a program that prints exactly
+ * "with proper grammar, but the outcome is a piece of art,"
+ * using the function printf
+ * return always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
