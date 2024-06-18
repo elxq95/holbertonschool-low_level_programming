2194d3604942f359c278d36f9f5ad7c470d30f41
elxq95 <leongxiqern12@gmail.com>
2024-06-18 04:38:56 +0000

    comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,22 +3,23 @@
  * main - Entry point
  *
  * Description: This is a C program that
- * prints values of base 16
+ * prints base 10 numbers in single
+ * digits with a "," and a space after
+ * each occuring number
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
-	char ch;
 
 	for (n = 0; n < 10; n++)
 	{
-		putchar(n + 48);
-	}
-	for (ch = 'a'; ch < 'g'; ch++)
-	{
-		putchar(ch);
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
