4286ac044f2eb24351fced65858f2f73b4ad8634
elxq95 <leongxiqern12@gmail.com>
2024-06-18 10:18:07 +0000

    101-print_comb4.c

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,27 @@
  * prints 3 digits
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int x, y, z;
-	
+
 	for (x = 0; x < 10; x++)
+	{
+		for (y = x + 1; y < 10; y++)
 		{
-			for (y = x + 1; y < 10; y++)
+			for (z = y + 1; z < 10; z++)
 			{
-				for (z = y + 1; z < 10; z++)
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				if (x != 7 || y != 8 || z != 9)
 				{
-					putchar(x + '0');
-					putchar(y + '0');
-					putchar(z + '0');
-					if ( x != 7 || y != 8 || z != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
