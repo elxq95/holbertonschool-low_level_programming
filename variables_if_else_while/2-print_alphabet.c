4d94d7e1062624073dc0a6bbd8874ec4f2def77e
elxq95 <leongxiqern12@gmail.com>
2024-06-18 03:00:35 +0000

    lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is a C program that prints
+ * alphabets in lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets = 'abcdefghijklmnopqrstuvwxyz';
+	putchar(alphabets);
+	putchar('\n');
+	return 0;
+}
