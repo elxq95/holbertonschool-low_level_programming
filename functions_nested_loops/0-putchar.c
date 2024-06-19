2df0525943c580214c5c579aaea2664e0fb49615
elxq95 <leongxiqern12@gmail.com>
2024-06-19 04:35:43 +0000

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This is a C program that
+ * prints _putchar using
+ * putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	{
+		putchar(95);
+		putchar(112);
+		putchar(117);
+		putchar(116);
+		putchar(99);
+		putchar(104);
+		putchar(97);
+		putchar(114);
+	}
+	putchar('\n');
+	return (0);
+}
