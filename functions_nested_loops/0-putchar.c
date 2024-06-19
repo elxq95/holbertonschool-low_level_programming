64c2466365df47cc727d9460d9be554b4edbb78c
elxq95 <leongxiqern12@gmail.com>
2024-06-19 04:44:20 +0000

    new

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,16 +9,6 @@
  */
 int main(void)
 {
-	{
-		putchar(95);
-		putchar(112);
-		putchar(117);
-		putchar(116);
-		putchar(99);
-		putchar(104);
-		putchar(97);
-		putchar(114);
-	}
-	putchar('\n');
+	
 	return (0);
 }
