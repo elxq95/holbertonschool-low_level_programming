5f84309d7f618ca94be4a52986e7373e2657067e
elxq95 <leongxiqern12@gmail.com>
2024-06-20 03:47:43 +0000

    n2

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -9,16 +9,12 @@
  */
 int _abs(int n)
 {
-	if (abs(n) > 0)
+	if (n = 0)
 	{
-		return (n);
-	}
-	else if (abs(n) < 0)
-	{
-		return (-n);
+		return (0);
 	}
 	else
 	{
-		return (0);
+		return (n);
 	}
 }
