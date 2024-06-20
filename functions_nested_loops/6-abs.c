279fd66b1938efc159f4cc2ca0fb3aadaab76240
elxq95 <leongxiqern12@gmail.com>
2024-06-20 03:36:46 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * _abs - check the code
+ * @int: check the code
+ * Return: 0 (Success)
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
