36602bb16e94a10943eb4c4ea8a75abc35673194
exlq95 <leongxiqern12@gmail.com>
2024-06-20 11:08:32 +0000

    push

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints numerical
+ * values to 98 only
+ * @n: value
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		if (n < 10)
+		{
+			_putchar(n + '0');
+		}
+		if (n >= 10)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
