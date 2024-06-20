72deaadb26d478e6de9a597c7d7836b41e7f92ff
exlq95 <leongxiqern12@gmail.com>
2024-06-20 10:45:12 +0000

    betty

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * add - add
+ * add - to add 2 integers
  * @a: integer
  * @b: integer
+ * Return: result
  */
-
 int add(int a, int b)
 {
 	int result;
-	
+
 	result = a + b;
 	return (result);
 }
