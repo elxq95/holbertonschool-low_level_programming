414ffdf6d82d6d4e379d971ae07fcd6e73477951
exlq95 <leongxiqern12@gmail.com>
2024-06-20 11:27:54 +0000

    11print

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,25 +7,36 @@
  */
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
+	if (n <= 98)
 	{
-		if (n < 10)
+		for (; n<= 98; n++)
 		{
-			_putchar(n + '0');
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		if (n >= 10)
+	}
+	else
+	{
+		for (; n >= 98; n--)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		if (n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		else	
-		{
-			_putchar('\n');
+			if (n ==98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
 	}
 }
