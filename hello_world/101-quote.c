5f1eaf9daab43aee569a57b540c6b8bd39ee3b2e
exlq95 <leongxiqern12@gmail.com>
2024-06-20 11:46:14 +0000

    quote

---
+++ hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - ");
+	putchar('D');
+	putchar('o');
+	putchar('r');
+	putchar('a');
+	putchar(' ');
+	putchar('K');
+	putchar('o');
+	putchar('r');
+	putchar('p');
+	putchar('a');
+	putchar('r');
+	putchar(',');
+	putchar(' ');
+	printf("2015-10-10");
+	return (1);
+}
