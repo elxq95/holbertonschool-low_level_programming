94c0065f311739b6ecdb5936264034981417af56
exlq95 <leongxiqern12@gmail.com>
2024-06-20 09:49:44 +0000

    timestable"

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - function that prints
+ * the 9 times table, starting with 0
+ *
+ * Return: check the code
+ */
+void times_table(void)
+{
+	int i, h;
+	int value;
+
+	value = h * i;
+
+	for (h = 0; h < 10;  h++)
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(value + '0');
+		{
+			if (h != 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
