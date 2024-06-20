c4ff2caf7fa96844ff984948b5d1cca221593de9
exlq95 <leongxiqern12@gmail.com>
2024-06-20 10:30:27 +0000

    fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,23 +12,32 @@ void times_table(void)
 	int value;
 
 
-	for (h = 0; h < 10;  h++)
+	for (h = 0; h <= 9;  h++)
 	{
-		for (i = 0; i < 10; i++)
+		for (i = 0; i <= 9; i++)
 		{
 			value = h * i;
 
-			if (i != 9)
+			if (i != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				if (value < 10)
+				{
+					_putchar(' ');
+				}
 			}
-			if (h < 10)
+			if (value >= 10)
+			{
+				_putchar((value / 10) + '0');
+				_putchar((value % 10) + '0');
+			}
+			else
 			{
 				_putchar(value + '0');
 			}
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
+
