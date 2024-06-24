4c098e90485f34bced2fd0169523f3dc2f05b4f0
root <elxq95>
2024-06-24 07:12:29 +0000

    isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <ctype.h>
-
 /**
  * _isdigit - checks for digit
  * @c: integer
@@ -11,15 +10,12 @@
 
 int _isdigit(int c)
 {
-	if (isdigit(c))
+	if (c >= 0 && c <= 9)
 	{
-		for (c = 0; c <= 9; c++)
-		{
-			return (1);
-		}
+		return (1);
 	}
 	else
 	{
-		return (0);
+		return 0;
 	}
-}
+}		
