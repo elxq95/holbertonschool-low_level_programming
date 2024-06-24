7ff337b10860c149d1ff48eddea669c0dc4f9352
root <elxq95>
2024-06-24 08:45:36 +0000

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplication of 2 variables
+ * @a: integer
+ * @b: integer
+ * Return : Always 0
+ */
+
+int mul(int a, int b)
+{
+	int value = a * b;
+
+	return (value);
+}
