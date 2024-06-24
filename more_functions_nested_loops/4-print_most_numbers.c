fb20ca8f5e7ad40c537db3030f2c55516f1da249
root <elxq95>
2024-06-24 09:27:23 +0000

    mostnumbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9
+ * except for 2 and 4
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (a = 2, 4)
+		continue;
+	}
+	_putchar(a + '0');
+	_putchar('\n');
+}	
