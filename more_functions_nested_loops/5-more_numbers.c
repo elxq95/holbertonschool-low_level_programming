9bd2fc364b5696457c6f797046cc67ed5f7816b7
root <elxq95>
2024-06-24 09:52:26 +0000

    morenumbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 14; a++)
+	{
+		b = 10;
+		{
+			_putchar((a + '0') * b);
+			{
+				while (a = 14)
+				{
+					_putchar('\n');
+				}
+			}
+		}
+
+	}
+}
