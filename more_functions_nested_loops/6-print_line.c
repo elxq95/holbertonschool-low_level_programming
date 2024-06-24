071e534c508ade2872e6d166bf78b53ac325e31a
root <elxq95>
2024-06-24 10:17:23 +0000

    printline

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: check the code
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	char a = '_';
+
+	if (n > 0)
+	{
+		_putchar(a * n);
+	}
+	else if (n < 0)
+	{
+		_putchar('\n');
+	}
+}
