d53490c9f82f90448c128710841d3daa87a86436
root <elxq95>
2024-06-24 09:36:47 +0000

    print-

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,12 +11,10 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++);
 	{
-		if (a != 2 && a != 4)
-		{
-			_putchar(a + '0');
-		}
-		_putchar('\n');
+		if (a != 2 && a != 4);
 	}
+	_putchar(a + '0');
+	_putchar('\n');
 }
