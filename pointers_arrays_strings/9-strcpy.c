834a3380da267e65f82425b74e74913ff8fd4f45
elxq95 <leongxiqern12@gmail.com>
2024-06-26 09:23:02 +0000

    fix

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -23,14 +23,3 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 }
-
-int main(void)
-{
-	char s1[98];
-	char *ptr;
-
-	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-    return (0);
-}
