0bdd39b30fc799660ee1874ec67f69bcff7bc8bb
elxq95 <leongxiqern12@gmail.com>
2024-06-27 12:20:21 +0000

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - function that compares
+ * two strings
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ * Return : finalnum
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int finalnum = 0;
+	int compare = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' & compare == 0)
+	{ 
+		if (s[i] != s2[i])
+		{
+			finalnum = s1[i] - s2[i];
+			compare = 1;
+		}
+		i++;
+	}
+	return (finalnum);
+}
