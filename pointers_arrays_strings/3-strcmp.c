d3655a2c58ca5ba44cc296024e4743c114ee7565
elxq95 <leongxiqern12@gmail.com>
2024-06-27 12:26:05 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * two strings
  * @s1: pointer to string 1
  * @s2: pointer to string 2
- * Return : finalnum
+ * Return: finalnum
  */
 
 int _strcmp(char *s1, char *s2)
@@ -16,17 +16,13 @@ int _strcmp(char *s1, char *s2)
 	int compare = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0' && compare == 0)
-	{ 
+	{
 		if (s1[i] != s2[i])
 		{
 			finalnum = s1[i] - s2[i];
 			compare = 1;
 		}
 		i++;
-	}
-	if (compare == 0)
-	{
-		finalnum = s1[i] - s2[i];
 	}
 	return (finalnum);
 }
