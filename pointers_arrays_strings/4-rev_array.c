6e1990904f6d2bfe3d6549a27518b0b12d0b9056
elxq95 <leongxiqern12@gmail.com>
2024-06-28 00:54:09 +0000

    rev

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses
+ * the content of an array
+ * of integers
+ * @a: pointer to an array of integers
+ * @n: number of elements of the array
+ * Return: (reva)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int start = 0;
+	int end = n - 1;
+	int reverse = 0;
+
+	if (n > 0)
+	{
+		reverse = 1;
+	}
+
+	while (start < end && reverse)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start ++;
+		end--;
+	}
+}
