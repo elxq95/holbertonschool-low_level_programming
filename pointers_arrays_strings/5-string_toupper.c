81f887834628eca33444b3c90a37b282e8bd8136
elxq95 <leongxiqern12@gmail.com>
2024-06-28 02:21:23 +0000

    stringtoupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -12,14 +12,12 @@
 char *string_toupper(char *str)
 {
 	int i = 0;
-	int convert = 0;
 
-	while (str[i] != '\0' && convert == 0)
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
-			convert = 1;
 		}
 		i++;
 	}
