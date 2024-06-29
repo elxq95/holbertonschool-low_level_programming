c56ab513c146bef790584e900faf3cad260a8b88
elxq95 <leongxiqern12@gmail.com>
2024-06-29 14:58:22 +0000

    7-leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,31 +10,22 @@
 
 char *leet(char *s)
 {
-	int i = 0;
+	char replacements[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char leet_chars[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int index = 0;
+	int replacementIndex;
 
-	while (s[i] != '\0')
+	while (s[index] != '\0')
 	{
-		if (s[i]  == 'a' || s[i] == 'A')
+		for (replacementIndex = 0; replacementIndex < 10; replacementIndex++)
 		{
-			s[i] = '4';
+			if (s[index] == replacements[replacementIndex])
+			{
+				s[index] = leet_chars[replacementIndex];
+				break;
+			}
 		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i]  = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] =  '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		i++;
+		index++;
 	}
 	return (s);
 }
