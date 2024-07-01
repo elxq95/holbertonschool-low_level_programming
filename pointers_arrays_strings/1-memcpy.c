c8b4ff93005273a4dd937be0015100c62e2e75cb
elxq95 <leongxiqern12@gmail.com>
2024-07-01 13:19:33 +0000

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -17,9 +17,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		while (j != 0);
 		{
 			src[i] = dest[j];
+			j++;
 		}
 		dest[j] = '\0';
 		i++;
