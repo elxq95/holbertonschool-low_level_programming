9143bfb1fca13f1945db3b18bb5c2bd9996e603b
elxq95 <leongxiqern12@gmail.com>
2024-07-02 10:31:52 +0000

    fix

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,23 +12,17 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0;
 	unsigned int j;
-	int findword = 0;
 	
 	while (haystack[i] != '\0')
 	{
 		j = 0;
-		findword = 1;
 		while (needle[j] != '\0')
 		{
 			if (haystack[i] == needle[j])
 			{
-				findword = 1;
+			return(needle);
 			}
 			j++;
-		}
-		if (findword == 1)
-		{
-			return (needle);
 		}
 		i++;
 	}
