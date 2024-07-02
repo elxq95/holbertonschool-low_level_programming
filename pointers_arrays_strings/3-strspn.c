d8ecc1cb02554cde36765e61ca98cec48b54a240
elxq95 <leongxiqern12@gmail.com>
2024-07-02 02:19:25 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,19 +5,38 @@
  * _strspn - function that gets the length
  * of a prefix substring
  * @s: pointer to a string
- * @accept: pointer to string 
+ * @accept: pointer to string
  * for the number of bytes
  * Return: s
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int counter = 0;
+	unsigned int byte = 0;
+	int charfound = 0;
+	int contloop = 1;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && contloop == 1)
 	{
-		s[i] = accept[i];
-		i++;
+		j = 0;
+		charfound = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				counter = counter + 1;
+				charfound = 1;
+			}
+			j = j + 1;
+		}
+		if (charfound == 0)
+		{
+			contloop = 0;
+		}
+		i = i + 1;
 	}
-	return (&s[i]);
+	return(counter);
 }
