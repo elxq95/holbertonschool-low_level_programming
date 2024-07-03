9317d5d5ee85413d5898086f0c7783c90c8b14ec
elxq95 <leongxiqern12@gmail.com>
2024-07-03 12:03:46 +0000

    test

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-#include "main.h"
+
+/**
+ * _puts_recursion - a function
+ * that prints a string followed
+ * by a new line
+ * @s: pointer to a string
+ * Return: Always 0
+ */
 
 void _puts_recursion(char *s)
 {
