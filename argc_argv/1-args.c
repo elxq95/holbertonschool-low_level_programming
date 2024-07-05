64edf9a537068ece62f23fa772b11bfa18b4870e
elxq95 <leongxiqern12@gmail.com>
2024-07-05 12:06:31 +0000

    hello

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - writes a program
+ * that prints the number of
+ * arguments passed into it
+ * @argc = number of arguments
+ * @argv = pointer to the arguments
+ * Return - Always 0
+ */
+
+int main(int argc, char *argv)
+{
+	
+		printf("%d\n", argc);
+		return (0);
+}
