aa14b5c33e80cedae882c5e3b263dbd2e2eb6954
elxq95 <leongxiqern12@gmail.com>
2024-07-05 03:28:31 +0000

    fix

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -16,7 +16,7 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return prime_helper(n, 2)
+	return prime_helper(n, 2);
 }
 
 /**
@@ -32,9 +32,9 @@ int prime_helper(int n, int divisor)
 	{
 		return (1);
 	}
-	if (n % divisor) == 0)
+	if (n % divisor  == 0)
 	{
 		return (0);
 	}
-	return prime_helper(n, divisor + 1)
+	return prime_helper(n, divisor + 1);
 }
