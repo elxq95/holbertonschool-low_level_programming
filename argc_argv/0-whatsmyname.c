fa2f2dd95bd3939275f5ec751a15bf84c4dbcaa3
elxq95 <leongxiqern12@gmail.com>
2024-07-05 11:51:42 +0000

    add

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * This is a program that
+ * prints its name
+ * followed by a new line
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
