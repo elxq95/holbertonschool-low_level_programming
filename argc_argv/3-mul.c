be02ca046ce56be403030973cd357508f98483e1
elxq95 <leongxiqern12@gmail.com>
2024-07-05 13:25:33 +0000

    fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,18 +14,18 @@ int main(int argc, char *argv[])
 	int value;
 	int a, b;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	value = a * b;
-
-	if (argc == 3)
-	{
-		printf("%d\n", value);
-		return (0);
-	}
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	else if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		value = a * b;
+		printf("%d\n", value);
+	}
+	return (0);
 }
