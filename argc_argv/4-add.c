121904d5c7be18e91910b99638a177c5ead6e85d
elxq95 <leongxiqern12@gmail.com>
2024-07-05 14:43:13 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				if (!isdigit(argv[i][j]))
+				if (!isdigit((unsigned char)argv[i][j]))
 				{
 					printf("Error\n");
 					return (1);
