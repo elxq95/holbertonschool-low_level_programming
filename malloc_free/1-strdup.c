5da84bc8958f881f2d3a83d138476a75b3dc23aa
elxq95 <leongxiqern12@gmail.com>
2024-07-08 09:47:52 +0000

    strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -26,7 +26,7 @@ char *_strdup(char *str)
 
 	for (length = 0; str[length] != 0; length++);
 
-	newstring = (char *)malloc((length +1) * sizeof(char));
+	newstring = (char *)malloc((length + 1) * sizeof(char));
 
 	if (newstring  == NULL)
 	{
