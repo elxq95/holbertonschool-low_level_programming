675d1ba78f8839ee731294a73c53aa99b863be39
elxq95 <leongxiqern12@gmail.com>
2024-07-08 11:14:52 +0000

    fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,9 +16,13 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, length1, length2;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	for (length1 = 0; s1[length1] != '\0'; length1++)
 	for (length2 = 0; s2[length2] != '\0'; length2++)
