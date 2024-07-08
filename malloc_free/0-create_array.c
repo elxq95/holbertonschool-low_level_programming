c516eaf0b4a59c5afa4fc35c972b5d3df7b87466
elxq95 <leongxiqern12@gmail.com>
2024-07-08 07:21:13 +0000

    push

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,6 +13,8 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
@@ -25,7 +27,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
