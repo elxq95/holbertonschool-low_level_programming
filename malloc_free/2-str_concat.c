9d3df0087e5e8b8310301b1b4feeeebcbadeeab0
elxq95 <leongxiqern12@gmail.com>
2024-07-08 11:17:55 +0000

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2 = '\0';
 	}
 
 	for (length1 = 0; s1[length1] != '\0'; length1++)
