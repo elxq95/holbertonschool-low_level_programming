5a47a201758ff42a7166c7b3241a75d6aec19d38
elxq95 <leongxiqern12@gmail.com>
2024-07-09 08:25:41 +0000

    malloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -10,6 +10,16 @@
  * memory
  */
 
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
+	void *ptr;
 
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return ptr;
+}
