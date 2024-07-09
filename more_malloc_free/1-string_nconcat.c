3fecbf8c35b775dbd1001bb230b728b616dad5c0
elxq95 <leongxiqern12@gmail.com>
2024-07-09 10:00:45 +0000

    string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,6 +7,7 @@
  * that concatenates two strings
  * @s1: pointer to string 1
  * @s2: pointer to string 2
+ * n: integer
  * Return: NULL if fail
  */
 
@@ -16,16 +17,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, length1, length2;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	for (length1 = 0; s1[length1] != '\0'; length1++)
+	{}
 	for (length2 = 0; length2 < n; length2++)
+	{}
 	{
 		if (n >= length2)
 		{
