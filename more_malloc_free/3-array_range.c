4ff8ec12cbec691a848316be5231a449345f001f
elxq95 <leongxiqern12@gmail.com>
2024-07-10 02:11:14 +0000

    unsigned int

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	array = malloc(sizeof(int) * (max - min + 1));
+	array = malloc(sizeof(unsigned int) * (max - min + 1));
 	if (array == NULL)
 	{
 		return (NULL);
