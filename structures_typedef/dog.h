1575f019a04de8bf0ac86d4d10c8494c1ba5e919
elxq95 <leongxiqern12@gmail.com>
2024-07-11 10:46:15 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,23 +1,10 @@
+#ifndef DOG_H
+#define DOG_H
+
 struct dog(
 {
         char *name;
         float *age;
         char *owner;
 };
-
-struct dog *new_dog(char *name, float *age, char *owner)
-{
-struct dog *newdog;
-
-dog = malloc(sizeof(struct dog));
-
-if (dog == NULL)
-{
-return (NULL);
-
-dog ->name = name;
-dog ->age = age;
-dog ->owner = owner;
-
-return dog;
-}
+#endif
