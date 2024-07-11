5995867cecebc62e0d4dbfa4a53f46ef3a838815
elxq95 <leongxiqern12@gmail.com>
2024-07-11 10:51:54 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - this is structure
+ * about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The ownder of the dog
+ */
+
 struct dog
 {
 	char *name;
