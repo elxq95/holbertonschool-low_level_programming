c378fb20ea17bf57b910df7df702d62e318aedca
elxq95 <leongxiqern12@gmail.com>
2024-07-12 01:14:39 +0000

    newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,7 +8,6 @@
  * @s: The string
  * Return: Length of the string
  */
-
 int _strlength(char *s)
 {
 	int length = 0;
@@ -19,16 +18,31 @@ int _strlength(char *s)
 	}
 	return (length);
 }
-
 /**
- * new_dog - function
- * that creates a new dog
+ * _strcopy - Copies a string to a destination buffer
+ * @dest: The destination buffer
+ * @src: The source string
+ *
+ * Return: Pointer to the destination buffer
+ */
+char *_strcpy(char *dest, char src)
+{
+	int i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
+/**
+ * new_dog - check the code
  * @name: name of dog
  * @age: age
  * @owner: owner
  * Return: Always 0
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
@@ -43,13 +57,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	strcpy(new_name, name);
+	_strcpy(new_name, name);
 
 	new_owner = malloc(_strlength(owner) + 1);
 	{
 		return (NULL);
 	}
-	strcpy(new_owner, owner);
+	_strcpy(new_owner, owner);
 
 	new_dog->name = new_name;
 	new_dog->age = age;
