1c13d7148a10d32a3d92740b5855a410f4736d85
elxq95 <leongxiqern12@gmail.com>
2024-07-12 00:57:52 +0000

    _

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	char *new_name, *new_owner;
 
-	new_dog = malloc(sizeof(dog_t_));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
