54acf63f0efcb869087942f73e83e0a0e258843b
elxq95 <leongxiqern12@gmail.com>
2024-07-12 00:54:57 +0000

    push

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - function
+ * that creates a new dog
+ * @name: name of dog
+ * @age: age
+ * @owner: owner
+ * Return - Always 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *new_name, *new_owner;
+
+	new_dog = malloc(sizeof(dog_t_));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_name = malloc(strlen(name) + 1);
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_name, name);
+
+	new_owner = malloc(strlen(owner) + 1);
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_owner, owner);
+
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = new_owner;
+
+	return (new_dog);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,5 +20,5 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *newdog(char *name, float age, char *owner);
 #endif
