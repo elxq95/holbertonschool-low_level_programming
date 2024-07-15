6012c9d2710cc94d04931fd76949f562c5d2eb97
elxq95 <leongxiqern12@gmail.com>
2024-07-15 01:46:24 +0000

    push

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name - function that
+ * prints a name
+ * @name: pointer to a name
+ * void (*f)(char*))
+ * Return: void
+ */
+
+void print_name(char *name, void(*f)(char *));
+{
+	ptr = f;
+
+	name = ptr();
+}
