0f1daf1729994cbd5d6bbb46808cea9ef5e8789e
elxq95 <leongxiqern12@gmail.com>
2024-07-16 04:20:35 +0000

    betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,10 +10,9 @@
  * Return: 0 if succesful
  */
 
-int (*get_op_func(char*s))(int, int)
+int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = 
-	{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -22,7 +21,7 @@ int (*get_op_func(char*s))(int, int)
 		{NULL, NULL}
 	};
 	int i;
-	
+
 	i = 0;
 	while (*ops[i].op != *s)
 	{
