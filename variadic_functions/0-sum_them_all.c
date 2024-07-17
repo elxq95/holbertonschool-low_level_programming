2dbf47470086982064dac671e4aa0a9204a32888
elxq95 <leongxiqern12@gmail.com>
2024-07-17 02:11:11 +0000

    push

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/** sum_them_all - function that returns the sum of all
+ * its parameters
+ * @n: const n
+ * Return: 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int total;
+	unsigned int i;
+	
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	total = 0;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		total +=  va_arg(args, int);
+	}
+
+	va_end(args);
+	return (total);
+}
---
+++ variadic_functions/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
