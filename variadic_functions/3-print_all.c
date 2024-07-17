2acfd926cb80bb5b01feea53c2a4784090421120
elxq95 <leongxiqern12@gmail.com>
2024-07-17 10:05:39 +0000

    repo

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+void print_all(const char * const format, ...)
+{
+	format_spec_t specs[] = {
+	{'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_string},
+        {'\0', NULL}
+	};
+	va_list args;
+	unsigned int i = 0, j;
+	char *separator = "";
+
+	va_start(args, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		j = 0;
+		while (specs[j].spec != '\0')
+		{
+			if (format[i] == specs[j].spec)
+			{
+				printf("%s",separator);
+				specs[j].print_func(args);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,19 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
+#include <stdarg.h>
 
+typedef struct format_spec {
+        char spec;
+        void (*print_func)(va_list args);
+} format_spec_t;
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
 #endif
