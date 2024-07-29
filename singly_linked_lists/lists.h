3aa447f6f6672a41bb145a931fe5910545fc1b20
elxq95 <leongxiqern12@gmail.com>
2024-07-29 05:04:24 +0000

    betty

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,12 +2,25 @@
 #define MY_HEADER
 #include <stddef.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
 struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 };
+
+/**
+ * list_t - Typedef for struct list_s
+ */
 
 typedef struct list_s list_t;
 
