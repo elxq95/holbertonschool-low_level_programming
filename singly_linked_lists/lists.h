f323b81e717bad98f4e53cbf14927f6ed7774e38
elxq95 <leongxiqern12@gmail.com>
2024-07-29 05:08:37 +0000

    finalbetty

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -11,21 +11,22 @@
  *
  * Description: singly linked list node structure
  */
-struct list_s {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 };
 
-/** 
- * list_t - Typedef for struct list_s 
+/**
+ * list_t - Typedef for struct list_s
  */
 typedef struct list_s list_t;
 
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the list_t list
- * 
+ *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h);
