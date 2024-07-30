dd993c18fcb936e2eb2a5e9dfc3a1cc284e8a107
elxq95 <leongxiqern12@gmail.com>
2024-07-30 05:27:31 +0000

    push

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <strings.h>
+
+/**
+ * add_dnodeint_end - function that adds
+ * a new node at the end of dlist
+ * @head: pointer to a pointer to the struct
+ * dlist
+ * @n: constant integer
+ * REturn: address of the new element, or NULL if failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *temp;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
+	new_node->prev = temp;
+
+	return(new_node);
+}
