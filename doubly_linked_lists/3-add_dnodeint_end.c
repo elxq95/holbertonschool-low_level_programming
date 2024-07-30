f4f1cca52adae83a432d5228dd4c67b29d40f15c
elxq95 <leongxiqern12@gmail.com>
2024-07-30 05:33:11 +0000

    push

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,12 +28,22 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	while (temp->next != NULL)
+
+	if (*head == NULL)
 	{
-		temp = temp->next;
+		new_node->prev = NULL;
+		*head = new_node;
 	}
-	temp->next = new_node;
-	new_node->prev = temp;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
 
 	return(new_node);
 }
