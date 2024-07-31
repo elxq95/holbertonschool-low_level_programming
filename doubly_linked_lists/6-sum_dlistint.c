e5ade4a8bb4700c016e3cf09e3ef6eac166ac1bf
elxq95 <leongxiqern12@gmail.com>
2024-07-31 01:29:54 +0000

    push

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - function that returns
+ * the sum of all the data (n) of a dlist
+ * @head: pointer to a struct element
+ * Return: 0 if list is empty
+ */
+
+int sum_dlistint(dlistitn_t *head)
+{
+	unsigned int sum, index = 0;
+
+	for (head != NULL)
+	{
+		sum += head;
+		return (sum);
+	}
+	head = head->next;
+	return (0);
+}
+
