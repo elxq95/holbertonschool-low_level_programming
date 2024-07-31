8009936e56c22ef7b67d0b4b2db51120c55dfce9
elxq95 <leongxiqern12@gmail.com>
2024-07-31 01:01:35 +0000

    betty

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,13 +12,13 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int current_index = 0; 
+	unsigned int current_index = 0;
 
 	while (head != NULL)
 	{
 		if (current_index == index)
 		{
-			return (head); 
+			return (head);
 		}
 		head = head->next;
 		current_index++;
