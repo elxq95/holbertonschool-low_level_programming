804723eb534f778f5dcb3173e6a8a38689fc92d1
elxq95 <leongxiqern12@gmail.com>
2024-08-03 07:47:27 +0000

    push

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,4 +10,25 @@
 
 void print_binary(unsigned long int n)
 {
-	
+       	unsigned long int mask = 1UL << 63;
+	int started = 0;
+
+    while (mask > 0)
+    {
+        if (n & mask != 0)
+        {
+            _putchar('1');
+            started = 1;
+        }
+        else if (started != 0 && (n & mask) == 0)
+        {
+            _putchar('0');
+        }
+        mask >>= 1;
+    }
+
+    if (started == 0)
+    {
+        _putchar('0');
+    }
+}
