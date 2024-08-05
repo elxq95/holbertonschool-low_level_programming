3a7f77fb2d84abba5a0100f8f9ea59343103c73b
elxq95 <leongxiqern12@gmail.com>
2024-08-05 03:02:38 +0000

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,59 +13,64 @@
  */
 int main(int argc, char *argv[])
 {
-	char *fone, *ftwo;
-	int fdone, fdtwo, sz = 1024, wr = 0, *buffer[1024];
+        char *fone, *ftwo;
+        int fdone, fdtwo, sz = 1024, wr = 0, *buffer[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+        if (argc != 3)
+        {
+                dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+                exit(97);
+        }
 
-	fone = argv[1];
-	ftwo = argv[2];
-	fdone = open(fone, O_RDONLY);
-	fdtwo = open(ftwo, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+        fone = argv[1];
+        ftwo = argv[2];
+        fdone = open(fone, O_RDONLY);
+        fdtwo = open(ftwo, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	if (fdone < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fone);
-		exit(98);
-	}
-	if (fdtwo < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ftwo);
-		exit(99);
-	}
+        if (fdone < 0)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fone);
+                exit(98);
+        }
+        if (fdtwo < 0)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ftwo);
+                close(fdone);  // Close the source file descriptor before exiting
+                exit(99);
+        }
 
-	while (sz != 0)
-	{
-		sz = read(fdone, buffer, 1024);
-		if (sz < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fone);
-			exit(98);
-		}
-		
-		wr = write(fdtwo, buffer, sz);
-		if (wr < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ftwo);
-			exit(99);
-		}
-	}
+        while (sz != 0)
+        {
+                sz = read(fdone, buffer, 1024);
+                if (sz < 0)
+                {
+                        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fone);
+                        close(fdone);  // Close the source file descriptor before exiting
+                        close(fdtwo);  // Close the destination file descriptor before exiting
+                        exit(98);
+                }
 
-	if (close(fdone) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdone);
-		exit(100);
-	}
+                wr = write(fdtwo, buffer, sz);
+                if (wr < 0)
+                {
+                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ftwo);
+                        close(fdone);  // Close the source file descriptor before exiting
+                        close(fdtwo);  // Close the destination file descriptor before exiting
+                        exit(99);
+                }
+        }
 
-	if (close(fdtwo) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdtwo);
-		exit(100);
-	}
+        if (close(fdone) < 0)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdone);
+                exit(100);
+        }
 
-	return (0);
+        if (close(fdtwo) < 0)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdtwo);
+                exit(100);
+        }
+
+        return (0);
 }
